4f68f2d735aac97b8141b498730af9b169b8e0ff
kunxin-chor <chorkunxin@yahoo.com>
2022-07-12 08:14:30 +0000

    Added ddl commands

---
+++ ddl.sql
@@ -0,0 +1,11 @@
+# Data Definition Language
+
+# create a database
+create database employees;
+
+# before we can issue commands for a database
+# we must set the current active database
+use employees;
+
+# show databases
+show databases;
