1411e6874bce94cca471c1621d30a274e84ce15b
kunxin-chor <chorkunxin@yahoo.com>
2022-07-19 08:41:07 +0000

    Added crud for categories

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ async function main() {
         'password': process.env.DB_PASSWORD
     })
 
+    app.get('/error', async function(req,res){
+        res.status(500);
+        res.send("Something went wrong");
+    })
+
     app.get('/actors', async function (req, res) {
         // connection.execute returns an array of results
         // the first element is the table that we selected
@@ -117,6 +122,79 @@ async function main() {
         const bindings = [parseInt(req.params.actor_id)]
         await connection.execute(query, bindings);
         res.redirect('/actors');
+    })
+
+    app.get('/categories', async function(req,res){
+        const query = "SELECT * FROM category ORDER BY name";
+        const [categories] = await connection.execute(query);
+        res.render('categories', {
+            'categories': categories
+        })
+    })
+
+    app.get('/categories/create', async function(req,res){
+        res.render('create_category')
+    })
+
+    app.post('/categories/create', async function(req,res){
+        let categoryName = req.body.name;
+        if (categoryName.length <= 25) {
+            const query = `INSERT INTO category (name) value (?)`
+            await connection.execute(query, [categoryName]);
+            res.redirect('/categories');
+        } else {
+            res.status(400);
+            res.send("Invalid request");
+        }
+    })
+
+    app.get('/categories/:category_id/update', async function(req,res){
+        // retrieve the category that we want to update
+        const query = "SELECT * from category where category_id = ?";
+        const bindings = [parseInt(req.params.category_id)];
+        try {
+            const [categories] = await connection.execute(query, bindings);
+            const category = categories[0];
+            res.render('update_category', {
+                'category':category
+            })
+        } catch(e) {
+
+        }
+    })
+
+    app.post('/categories/:category_id/update', async function(req,res){
+        try {
+            const query = `UPDATE category SET name=? WHERE category_id = ?`;
+            const bindings = [req.body.name, parseInt(req.params.category_id)]
+            await connection.execute(query, bindings);
+            res.redirect('/categories');
+        } catch(e) {
+            console.log(e);
+            res.redirect('/error')
+        }
+     
+    })
+
+    app.get('/categories/:category_id/delete', async function(req,res){
+        const query = "SELECT * from category where category_id = ?"
+        const [categories] = await connection.execute(query, [parseInt(req.params.category_id)]);
+        const categoryToDelete = categories[0]; 
+        res.render('confirm_delete_category',{
+            'category': categoryToDelete
+        })
+    })
+
+    app.post('/categories/:category_id/delete', async function(req,res){
+        
+        // find all the films which have category_id equal to the one that we are trying to delete
+        const deleteQuery = "DELETE FROM film_category where category_id = ?";
+        await connection.execute(deleteQuery, [parseInt(req.params.category_id)]);
+       
+        const query = "DELETE FROM category where category_id = ?";
+        const bindings = [parseInt(req.params.category_id)];
+        await connection.execute(query, bindings);
+        res.redirect('/categories');
     })
 }
 main();
