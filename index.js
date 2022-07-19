689990705ecbe15ba2438d509ec2c5815b907146
kunxin-chor <chorkunxin@yahoo.com>
2022-07-19 06:22:36 +0000

    Added update annd create

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const hbs = require('hbs');
 const wax = require('wax-on');
 require('dotenv').config();
 const mysql2 = require('mysql2/promise');  // to use await/async, we must
-                                          // use the promise version of mysql2
+// use the promise version of mysql2
 
 
 
@@ -11,6 +11,10 @@ const app = express();
 app.set('view engine', 'hbs');
 wax.on(hbs.handlebars);
 wax.setLayoutPath('./views/layouts')
+
+app.use(express.urlencoded({
+    'extended': false
+}));
 
 
 async function main() {
@@ -20,8 +24,8 @@ async function main() {
         'database': process.env.DB_DATABASE,
         'password': process.env.DB_PASSWORD
     })
-  
-    app.get('/actors', async function(req,res){
+
+    app.get('/actors', async function (req, res) {
         // connection.execute returns an array of results
         // the first element is the table that we selected
         // the second element onwards are some housekeeping data
@@ -31,20 +35,20 @@ async function main() {
         // short form for:
         // const results = await connection.execute("SELECT * FROM actor");
         // const actors = results[0];
-        
-        res.render('actors.hbs',{
+
+        res.render('actors.hbs', {
             'actors': actors
         })
     })
 
-    app.get('/staff', async function (req,res){
+    app.get('/staff', async function (req, res) {
         const [staff] = await connection.execute('SELECT staff_id, first_name, last_name, email from staff');
-        res.render('staff',{
+        res.render('staff', {
             'staff': staff
         })
     })
 
-    app.get('/search', async function(req,res){
+    app.get('/search', async function (req, res) {
 
         // define the 'get all results query'
         let query = "SELECT * from actor WHERE 1";
@@ -63,14 +67,49 @@ async function main() {
             query += ` AND last_name LIKE ?`;
             bindings.push('%' + req.query.last_name + '%')
         }
-
-        console.log(query, bindings);
         let [actors] = await connection.execute(query, bindings);
 
-        res.render('search',{
-            'actors':actors
+        res.render('search', {
+            'actors': actors
         })
 
+    })
+
+    app.get('/actors/create', async function (req, res) {
+        res.render('create_actor');
+    })
+
+    app.post('/actors/create', async function (req, res) {
+        const query = "insert into actor (first_name, last_name) values (?, ?)";
+        const bindings = [req.body.first_name, req.body.last_name];
+        await connection.execute(query, bindings);
+        res.redirect('/actors');
+    })
+
+    app.get('/actors/:actor_id/update', async function (req, res) {
+        const actorId = parseInt(req.params.actor_id);
+        const query = "select * from actor where actor_id = ?";
+        const [actors] = await connection.execute(query, [actorId]);
+        const actorToUpdate = actors[0]; // since we are only expecting one result,we just take the first index
+        res.render('update_actor',{
+            'actor': actorToUpdate
+        })
+    })
+
+    app.post('/actors/:actor_id/update', async function(req,res){
+
+        if (req.body.first_name.length > 45 || req.body.last_name > 45) {
+            res.status(400);
+            res.send("Invalid request");
+            return;
+        }
+
+        // sample query
+        // update actor set first_name="Zoe2", last_name="Tay2" WHERE actor_id = 202;
+        const query = `update actor set first_name=?, last_name=? WHERE actor_id = ?`
+        const bindings = [req.body.first_name, req.body.last_name, parseInt(req.params.actor_id)];
+        await connection.execute(query, bindings);
+        res.redirect('/actors');
     })
 }
 main();
@@ -80,6 +119,6 @@ app.use(express.urlencoded({
     'extended': false
 }))
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("server has started")
 })
